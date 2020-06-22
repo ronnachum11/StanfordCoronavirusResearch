2ddc64c093a1c087069593eb91959a73aaf35bae
Logan Pageler <pagelerlogan@gmail.com>
2020-06-22 13:15:04 -0700

    Created program to update NYC files

---
+++ Hospitalization Data/New York/NYCGovDataUpdater.py
@@ -0,0 +1,6 @@
+import urllib.request
+
+print('Beginning file download with urllib2...')
+
+url = 'https://raw.githubusercontent.com/nychealth/coronavirus-data/master/boro/boroughs-case-hosp-death.csv'
+urllib.request.urlretrieve(url, 'NYCGovUpdater.csv')
