69a254e86acf2fa6a574ddf1b7e7363e708a8745
Ron <gokepler@gmail.com>
2020-07-13 14:55:21 -0400

    Added Doubling Time Script

--- Scripts/ron_doubling_time.py
+++ Scripts/ron_doubling_time.py
@@ -2,6 +2,7 @@ import pandas as pd
 import os
 import math
 import matplotlib.pyplot as plt
+import numpy as np 
 
 states_dict = {
     'Alabama': 'AL',
@@ -63,15 +64,37 @@ states_dict = {
 }
 path = "C:\\Users\\Ron\\StanfordCoronavirusResearch"
 
+def doubling_time(m, y, window):
+    y1, y2 = y[m - window], y[m]
+
+    return (window) * np.log(2) / np.log(y2 / y1)
+
 for state in states_dict:
     if state == "Florida":
         state_data = pd.read_csv(os.path.join(path, "RawData", state, f"{states_dict[state].lower()}_covid_track_api_data.csv"))
 
         hospitalized = list(state_data['hospitalizedCumulative'])[::-1]
+        dates = list(state_data['date'])[::-1]
+        
+        num, count = hospitalized[0], 0
+        while math.isnan(num):
+            num = hospitalized[count]
+            count += 1
+        hospitalized, dates = hospitalized[count:], dates[count:]
 
-        for i in range(1, len(hospitalized)):
-            if math.isnan(hospitalized[i]):
-                hospitalized[i] = hospitalized[i-1]
+        window = 14
+        doubling_time = [0] * window + [doubling_time(x, hospitalized, window) for x in range(window, len(hospitalized))]
 
-        plt.plot(hospitalized)
+        x_ticks, x_tick_labels = [], []
+        for i in range(0, len(dates), len(dates)//7 - 1):
+            date = str(dates[i])
+            x_ticks.append(i)
+            x_tick_labels.append(date[4:6] + "/" + date[6:8])
+        plt.xticks(x_ticks, x_tick_labels)
+        plt.xlabel("Dates")
+        plt.ylabel("Doubling Time (Days)")
+        plt.title(f"COVID-19 Hospitalization Doubling Time - {state}\nUsed covidtracking.com/api - Some data may be innacurate")
+        plt.plot(doubling_time, label="Doubling Time")
+        plt.legend()
         plt.show()
+        plt.cla()
