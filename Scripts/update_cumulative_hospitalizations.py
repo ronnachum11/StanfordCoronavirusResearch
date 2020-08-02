36d7621e29a72320c13206c6f4a63912b6f62477
Logan Pageler <pagelerlogan@gmail.com>
2020-08-02 14:22:01 -0700

    ConvertCurrentToTotal update

--- Scripts/update_cumulative_hospitalizations.py
+++ Scripts/update_cumulative_hospitalizations.py
@@ -138,8 +138,8 @@ for state in states:
     else:
         plt.title(f"COVID-19 Cumulative Hospitalizations (Calculated) - {state}")
     plt.plot(hospitalized, color='k')
-    plt.savefig(os.path.join("Graphs", "General", "Cumulative Hospitalizations", f"{state}.png"), bbox_inches='tight')
-    plt.savefig(os.path.join("Graphs", "Analysis", states_dict[state], "2CumulativeHospitalizations.png"), bbox_inches='tight')
+    plt.savefig(os.path.join("../Graphs", "General", "Cumulative Hospitalizations", f"{state}.png"), bbox_inches='tight')
+    plt.savefig(os.path.join("../Graphs", "Analysis", states_dict[state], "2CumulativeHospitalizations.png"), bbox_inches='tight')
     # plt.show()
     plt.cla()
 # print(len(states))
