c87970895cc19c4c2be405ee61c832a2f281bd8d
Ron <gokepler@gmail.com>
2020-08-13 23:51:23 -0400

    editted procfile

--- application/routes/main_routes.py
+++ application/routes/main_routes.py
@@ -86,7 +86,7 @@ def research():
 @app.route("/graphs", methods=["GET"])
 def graphs():
     path = "C:\\Users\\Ron\\StanfordCoronavirusResearch"
-    graph_folder = os.path.join(app.config["PATH"], 'Website', 'application', 'static', 'graphs', 'usa')
+    graph_folder = os.path.join('application', 'static', 'graphs', 'usa')
     graph_names = ['hospitalization_increase.html']
     graphs = []
     for graph in graph_names:
